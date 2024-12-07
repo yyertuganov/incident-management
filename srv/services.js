aa77ed426290825129e039fe47d03814c22af02f
Yergali Yertuganov <yergali.yertuganov@jungheinrich.es>
2024-12-07 11:30:47 +0000

    Initial Push

---
+++ srv/services.js
@@ -0,0 +1,43 @@
+const cds = require('@sap/cds')
+
+class ProcessorService extends cds.ApplicationService {
+  /** Registering custom event handlers */
+  init() {
+    this.before("UPDATE", "Incidents", (req) => this.onUpdate(req));
+    this.before("CREATE", "Incidents", (req) => this.changeUrgencyDueToSubject(req.data));
+    this.after("READ",(req) => this.onRead(req));
+
+    return super.init();
+  }
+
+  changeUrgencyDueToSubject(data) {
+    if (data) {
+      const incidents = Array.isArray(data) ? data : [data];
+      incidents.forEach((incident) => {
+        if (incident.title?.toLowerCase().includes("urgent")) {
+          incident.urgency = { code: "H", descr: "High" };
+        }
+      });
+    }
+  }
+
+  onRead(data) {
+    if (data) {
+      const incidents = Array.isArray(data) ? data : [data];
+      incidents.forEach((incident) => {
+        if (incident.title?.toLowerCase().includes("yy")) {
+          let temp = incident.title + " Yergali"
+          incident.title = temp;
+        }
+      });
+    }
+  }
+
+  /** Custom Validation */
+  async onUpdate (req) {
+    const { status_code } = await SELECT.one(req.subject, i => i.status_code).where({ID: req.data.ID})
+    if (status_code === 'C')
+      return req.reject(`Can't modify a closed incident`)
+  }
+}
+module.exports = { ProcessorService }
